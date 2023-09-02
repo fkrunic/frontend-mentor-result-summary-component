9a0df53b9a59bd7d9e905533aff53272f05b302b
Filip Krunic <fkrunic9@gmail.com>
2023-09-02 16:14:14 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Result Summary Component
+Implementation of [Result Summary Component](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV) using Vue and TailwindCSS.
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+</style>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      bodyClass: ''
+    };
+  },
+  mounted() {
+    document.body.className = this.bodyClass;
+  },
+  beforeDestroy() {
+    // Reset the class when the component is destroyed
+    document.body.className = '';
+  }
+};
+</script>
