499991e6298761f42e89d273182392736f1d32d2
Filip Krunic <fkrunic9@gmail.com>
2023-09-02 17:01:53 -0700

    structural groupings

--- src/App.vue
+++ src/App.vue
@@ -4,89 +4,109 @@
 <template>
   <div class="flex flex-col justify-start items-center min-h-screen">
 
-    <!-- Banner -->
-    <div>
-      <p>Your Result</p>
+    <!-- Card -->
+    <div class="flex flex-col">
 
-      <!-- Score Callout -->
-      <div>
-        <p>76</p>
-        <p>of 100</p>
+      <!-- Banner -->
+      <div class="
+        flex 
+        flex-col 
+        items-center
+
+        bg-gradient-to-b from-light-slate-blue to-light-royal-blue
+        ">
+
+
+        <p class="font-bold text-light-lavender">Your Result</p>
+
+        <!-- Score Callout -->
+        <div class="
+          flex 
+          flex-col 
+          items-center
+          
+          bg-gradient-to-b from-violet-blue to-persian-blue
+          
+          ">
+          <p>76</p>
+          <p>of 100</p>
+        </div>
+
+        <div class="flex flex-col items-center">
+          <p>Great</p>
+          <p>
+            You scored higher than 65% of the people who have taken these tests.
+          </p>        
+        </div>
       </div>
 
-      <p>Great</p>
+      <!-- Content -->
+      <div class="flex flex-col">
+        <p>Summary</p>
 
-      <p>
-        You scored higher than 65% of the people who have taken these tests.
-      </p>
+        <!-- Scores -->
+        <div>
+          
+          <!-- Reaction Score -->
+          <div class="flex flex-row">
+            <div class="flex flex-row">
+              <img src="./assets/img/icon-reaction.svg" alt="reaction">
+              <p>Reaction</p>
+            </div>
+            <div class="flex flex-row">
+              <p>80</p>
+              <p>/ 100</p>
+            </div>
+          </div>
+
+          <!-- Memory Score -->
+          <div class="flex flex-row">
+            <div class="flex flex-row">
+              <img src="./assets/img/icon-memory.svg" alt="memory">
+              <p>Memory</p>
+            </div>
+            <div class="flex flex-row">
+              <p>92</p>
+              <p>/ 100</p>
+            </div>
+          </div>
+          
+          <!-- Verbal Score -->
+          <div class="flex flex-row">
+            <div class="flex flex-row">
+              <img src="./assets/img/icon-verbal.svg" alt="verbal">
+              <p>Verbal</p>
+            </div>
+            <div class="flex flex-row">
+              <p>61</p>
+              <p>/ 100</p>
+            </div>
+          </div>
+          
+          <!-- Visual Score -->
+          <div class="flex flex-row">
+            <div class="flex flex-row">
+              <img src="./assets/img/icon-visual.svg" alt="visual">
+              <p>Visual</p>
+            </div>
+            <div class="flex flex-row">
+              <p>72</p>
+              <p>/ 100</p>
+            </div>
+          </div>        
+
+        </div>
+
+        <!-- Button -->
+        <div class="bg-dark-gray-blue">
+          <p class="text-pale-blue">Continue</p>
+        </div>
+
+      </div>
 
     </div>
-
-    <!-- Content -->
-    <div>
-      <p>Summary</p>
-
-      <!-- Scores -->
-      <div>
-        
-        <!-- Reaction Score -->
-        <div class="flex flex-row">
-          <div class="flex flex-row">
-            <img src="./assets/img/icon-reaction.svg" alt="reaction">
-            <p>Reaction</p>
-          </div>
-          <div class="flex flex-row">
-            <p>80</p>
-            <p>/ 100</p>
-          </div>
-        </div>
-
-        <!-- Memory Score -->
-        <div class="flex flex-row">
-          <div class="flex flex-row">
-            <img src="./assets/img/icon-memory.svg" alt="memory">
-            <p>Memory</p>
-          </div>
-          <div class="flex flex-row">
-            <p>92</p>
-            <p>/ 100</p>
-          </div>
-        </div>
-        
-        <!-- Verbal Score -->
-        <div class="flex flex-row">
-          <div class="flex flex-row">
-            <img src="./assets/img/icon-verbal.svg" alt="verbal">
-            <p>Verbal</p>
-          </div>
-          <div class="flex flex-row">
-            <p>61</p>
-            <p>/ 100</p>
-          </div>
-        </div>
-        
-        <!-- Visual Score -->
-        <div class="flex flex-row">
-          <div class="flex flex-row">
-            <img src="./assets/img/icon-visual.svg" alt="visual">
-            <p>Visual</p>
-          </div>
-          <div class="flex flex-row">
-            <p>72</p>
-            <p>/ 100</p>
-          </div>
-        </div>        
-
-      </div>
-
-      <!-- Button -->
-      <div class="bg-dark-gray-blue">
-        <p class="text-pale-blue">Continue</p>
-      </div>
-
-    </div>
-
   </div>
+
 </template>
 
 <style scoped>
@@ -99,7 +119,7 @@
 export default {
   data() {
     return {
-      bodyClass: ''
+      bodyClass: 'font-hanken-grotesk'
     };
   },
   mounted() {
